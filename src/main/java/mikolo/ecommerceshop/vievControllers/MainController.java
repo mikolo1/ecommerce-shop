beaf8b8919f6554d2e920c885c5f3e2330f656c9
Mikolaj Kedzierski <mikked@poczta.onet.pl>
2019-10-27 16:01:53 +0100

    minor changes

--- src/main/java/mikolo/ecommerceshop/vievControllers/MainController.java
+++ src/main/java/mikolo/ecommerceshop/vievControllers/MainController.java
@@ -1,7 +1,9 @@
 package mikolo.ecommerceshop.vievControllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
