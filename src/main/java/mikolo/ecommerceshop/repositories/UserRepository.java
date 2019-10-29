5c5d72537074f4169cd90270e89151464f0b6327
Mikolaj Kedzierski <mikked@poczta.onet.pl>
2019-10-29 19:02:32 +0100

    Register and login form internacjonalization added

--- src/main/java/mikolo/ecommerceshop/repositories/UserRepository.java
+++ src/main/java/mikolo/ecommerceshop/repositories/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository <User, Long> {
-    public Optional<User> findUserByEmail(String email);
-    public User findByEmail(String email);
+    Optional<User> findUserByEmail(String email);
+    User findByEmail(String email);
 }
