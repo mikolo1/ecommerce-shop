21c63ece235edfca4cb225ceae1a6bf0a301bf66
Mikolaj Kedzierski <mikked@poczta.onet.pl>
2019-11-04 17:30:20 +0100

    minor changes

--- src/main/java/mikolo/ecommerceshop/utils/Consts.java
+++ src/main/java/mikolo/ecommerceshop/utils/Consts.java
@@ -2,5 +2,5 @@ package mikolo.ecommerceshop.utils;
 
 public class Consts {
     public final static String EMAIL_PATTERN = "^[^_][\\w]+[\\.\\w]*@[\\w]+(\\.[a-zA-Z]{2,})+$";
-    public final static String PASSWORD_PATTERN = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\!\\@\\#\\$\\*\\_])(?!.*\\s).{8,12}$";
+    public final static String PASSWORD_PATTERN = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\!\\@\\#\\$\\*\\_])(?!.*\\s).{8,12}$";
 }
